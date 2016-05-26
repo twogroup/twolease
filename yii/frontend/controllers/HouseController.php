96e6e7f4cd04b9f60041ca15a5a96021945aadc9
w_weisen <w54supersen@163.com>
2016-05-26 21:04:56 +0800

    sen

--- yii/frontend/controllers/HouseController.php
+++ yii/frontend/controllers/HouseController.php
@@ -16,7 +16,7 @@ class HouseController extends \yii\web\Controller
         return $this->render('index',array('ast'=>$posts));
     }
     //单删房源信息
-    public function actionDel(){
+    public function actionHousedelete(){
     	$connection = \Yii::$app->db;
     	$request = Yii::$app->request;
     	$id = $request->get('id');
@@ -35,6 +35,41 @@ class HouseController extends \yii\web\Controller
     	$command[] = $connection->createCommand('SELECT * FROM house WHERE rent_id='.$id)->queryOne();
     	//print_r($command);die; 
     	return $this->render('details',array('arr'=>$command));
+    }
+    //修改房源信息
+    public function actionHouseupload(){
+        $connection = \Yii::$app->db;
+        $request = Yii::$app->request;
+        $id = $request->get('id');
+        $command[] = $connection->createCommand('SELECT * FROM house WHERE rent_id='.$id)->queryOne();
+        return $this->render('houseupload',array('arr'=>$command));
+    }
+    public function actionUploadadd(){
+        $connection = \Yii::$app->db;
+        $request = Yii::$app->request;
+        $data = $request->post();
+        //print_r($data);die;
+        $id = $data['id'];
+        $contacts = $data['rent_people'];
+        //print_r($contacts);die;
+        $community = $data['by_rent_people'];
+        $area = $data['house_type'];
+        $pay = $data['mon_rental'];
+        $content = $data['property_class'];
+        $phone = $data['rent_money'];
+        $uploadOk = $connection->createCommand()->update('house', [
+            'contacts' => $contacts,
+            'community' => $community,
+            'area' => $area,
+            'pay' => $pay,
+            'content' => $content,
+            'phone' => $phone,
+        ], "rent_id = '$id'")->execute();
+        if ($uploadOk) {
+            return $this->redirect('index.php?r=house/houselist');
+        }else{
+            echo "<script>alert('抱歉！提交失败');location.href='index.php?r=house/houselist</script>";
+        }
     }
 
 }
