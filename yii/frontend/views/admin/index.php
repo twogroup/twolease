0c21a67149d40973e87261b8c4465754b8c4e313
wrx1230 <1223215104@qq.com>
2016-05-26 13:46:22 +0800

    11

--- yii/frontend/views/admin/index.php
+++ yii/frontend/views/admin/index.php
@@ -276,7 +276,7 @@
 								<img class="nav-user-photo" src="assets/avatars/user.jpg" alt="Jason's Photo" />
 								<span class="user-info">
 									<small>欢迎光临,</small>
-									Jason
+									<?php echo $name; ?>
 								</span>
 
 								<i class="icon-caret-down"></i>
@@ -300,7 +300,7 @@
 								<li class="divider"></li>
 
 								<li>
-									<a href="#">
+									<a href="index.php?r=admin/exits">
 										<i class="icon-off"></i>
 										退出
 									</a>
