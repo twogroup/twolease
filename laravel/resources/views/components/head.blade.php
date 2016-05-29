530675247bec8e3547cd5f23f3bde1b50a5600f5
wrx1230 <1223215104@qq.com>
2016-05-29 18:50:28 +0800

    王瑞雪

--- laravel/resources/views/components/head.blade.php
+++ laravel/resources/views/components/head.blade.php
@@ -3,10 +3,14 @@
             <li><a class="active scroll" href="{{url('')}}"><i class="fa fa-home"> </i>首页</a></li>
             <li><a href="{{url('service')}}"><i class="fa fa-thumbs-up"> </i>房源</a></li>
             <!--判断是用户还是房东-->
-            <?php if($_COOKIE['status']!=2){?>
-
+            <?php if(empty($_COOKIE['status'])){?>
+                <li><a href="{{url('lease')}}"><i class="fa fa-home"> </i>我要出租</a></li>
             <?php }else{?>
-            <li><a href="{{url('lease')}}"><i class="fa fa-home"> </i>我要出租</a></li>
+                <?php if($_COOKIE['status']!=2){?>
+
+                <?php }else{?>
+                <li><a href="{{url('lease')}}"><i class="fa fa-home"> </i>我要出租</a></li>
+                <?php }?>
             <?php }?>
             <!--The end-->
             {{--<li><a href="{{url('gallery')}}"><i class="fa fa-picture-o"> </i>画廊</a></li>--}}
